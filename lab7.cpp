425bf908681288d821e7d453a4254c885da29c4c
3rase <136752556+akit3ru@users.noreply.github.com>
2024-12-16 01:12:47 +0200

    lab 7

---
+++ lab7.cpp
@@ -0,0 +1,25 @@
+﻿#include "func.hpp"
+#include <iostream>
+
+int main()
+{
+    int mat[100][100] = { 0 };
+    int n;
+    std::cin >> n;
+    matrixCreation(mat, n);
+
+    if (sameRows) 
+    {
+        replaceMinimalElements(mat, n, max_prime_element(mat, n));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                std::cout << mat[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+
+    }
+
+    return 0;
+}
